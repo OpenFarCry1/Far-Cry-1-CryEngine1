ba3f7ba987e44ba6f9414f1d27bcffe0e4bf453d
IDL_crynet <dk7289057@gmail.com>
2023-07-13 18:19:22 +0300

    updated: ProjectDefines.h

--- CryCommon/ProjectDefines.h
+++ CryCommon/ProjectDefines.h
@@ -30,13 +30,13 @@
 #else
 	
 	#define _DATAPROBE
-//	#define NOT_USE_UBICOM_SDK					// mainly needed for licencees to compile without the UBI.com integration
+	#define NOT_USE_UBICOM_SDK					// mainly needed for licencees to compile without the UBI.com integration
 
-//	#define NOT_USE_PUNKBUSTER_SDK				// mainly needed for licencees to compile without the Punkbuster integration
-//	#define NOT_USE_BINK_SDK					// mainly needed for licencees to compile without the Bink integration
-//	#define NOT_USE_DIVX_SDK					// mainly needed for licencees to compile without the DivX integration
-//	#define NOT_USE_ASE_SDK						// mainly needed for licencees to compile without the ASE integration
-//	#define EXCLUDE_UBICOM_CLIENT_SDK			// to compile a standalone server without the client integration
+	#define NOT_USE_PUNKBUSTER_SDK				// mainly needed for licencees to compile without the Punkbuster integration
+	#define NOT_USE_BINK_SDK					// mainly needed for licencees to compile without the Bink integration
+	#define NOT_USE_DIVX_SDK					// mainly needed for licencees to compile without the DivX integration
+	#define NOT_USE_ASE_SDK						// mainly needed for licencees to compile without the ASE integration
+	#define EXCLUDE_UBICOM_CLIENT_SDK			// to compile a standalone server without the client integration
 
 #endif //LINUX
 
